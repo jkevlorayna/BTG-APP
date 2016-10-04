d43fca06681783d6a76a36c17dbd588274a929da
kevin <jkevlorayna@hotmail.com>
2016-10-04 23:31:48 +0800

    fixes

--- core/header.php
+++ core/header.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
-	<title>Nergos Island Travel Guide Guide Administrator</title>
+	<title>Negros Island Travel Guide Guide Administrator</title>
 	<!-- Bootstrap core CSS -->
 
 	<link href="<?php echo $path; ?>css/bootstrap.css" rel="stylesheet">
